c35453b9bd44a365a2c461467ef3d21a218e231c
gepals18 <palsg@my.erau.edu>
2026-01-22 16:16:52 -0500

    Refactor string length calculation and input handling
    Fix string input handling and improve length calculation.

--- myStringLenGP.c
+++ myStringLenGP.c
@@ -1,12 +1,23 @@
 //Gretchen Pals Quiz 1
 #include <stdio.h>
+int findLength(char *strPtr);
+
 int main (int argc, char * argv[])
 {
-  char string[];
-  printf("enter a string");
-  scanf("%s", string[]);
-  while(*(lettersPtr+i))
+  int i = 1; 
+  if (argc > 1) 
+  { 
+    int len = findLength(argv[i]); 
+    printf("Length: %d\n", len); 
+  } 
+  return 0;
+}
+int findLength (char * strPtr)
+{
+int len = 0;
+  while(strPtr[len] != '\0')
   {
-    
+    len++;
   }
+return len;
 }
