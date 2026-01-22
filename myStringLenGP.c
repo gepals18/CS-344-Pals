f06336e32a2e037d0e529994e14d08e2d892a1f5
gepals18 <palsg@my.erau.edu>
2026-01-22 16:05:35 -0500

    Create myStringLenGP.c for string length calculation
    Initialize myStringLenGP.c with basic structure for string input.

---
+++ myStringLenGP.c
@@ -0,0 +1,12 @@
+//Gretchen Pals Quiz 1
+#include <stdio.h>
+int main (int argc, char * argv[])
+{
+  char string[];
+  printf("enter a string");
+  scanf("%s", string[]);
+  while(*(lettersPtr+i))
+  {
+    
+  }
+}
